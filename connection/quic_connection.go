1987aca47aa418a9f3192b5e6a3a16098e0ab8e4
Michael Kamensky <mkamensk@ucsc.edu>
2025-04-07 16:24:03 -0700

    goodnews we added the ssl key logging so now we can see the packets
    other good news I think I know the problem the server sends a response to go to a different stream and then closes the original one and we need to swtich to stream 3

--- connection/quic_connection.go
+++ connection/quic_connection.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"os"
 	"log"
 	"net"
 	"time"
@@ -26,12 +27,11 @@ type QUICConnection struct {
 
 // NewQUICConnection establishes a new QUIC connection and ensures handshake completion
 func NewQUICConnection(host string, port uint) *QUICConnection {
-	// TO DO: change from returning nil to raising an error 
-
+	// TO DO: change from returning nil to raising an error
 	// TO DO: stop hard coding of the port
 	host = host + ":443"
 
-	// getting the server addr with udp 
+	// getting the server addr with udp
 	server_addr, err := net.ResolveUDPAddr("udp", host)
 	if err != nil {
 		fmt.Println("Error resolving domain:", err)
@@ -52,37 +52,40 @@ func NewQUICConnection(host string, port uint) *QUICConnection {
 		return nil
 	}
 
-	// my tls confid
+	// Open or create the SSL keylog file
+	keyLogFile, err := os.OpenFile("/home/mike/Documents/sslkeylog/sslkeylogfile.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening SSL keylog file:", err)
+		return nil
+	}
+	defer keyLogFile.Close()
+
+	// my tls config with KeyLogWriter for SSL key logging
 	tlsConfig := &tls.Config{
-		// TO DO: don't skip the verification in production
-		InsecureSkipVerify: true,             // For testing purposes; skip verification
-		MinVersion:         tls.VersionTLS13, 
-		NextProtos:         []string{"h3"},   
+		// Don't skip verification in production; only for testing
+		InsecureSkipVerify: true,
+		MinVersion:         tls.VersionTLS13,
+		NextProtos:         []string{"h3"},
+		KeyLogWriter:       keyLogFile, // Log the SSL keys to the specified file
 	}
 
 	// Create a QUIC configuration
 	quicConfig := &quic.Config{
-		MaxIdleTimeout: 10 * time.Second,
+		MaxIdleTimeout:   10 * time.Second,
 		MaxIncomingStreams: 1000,
 	}
 
+	// Establish QUIC connection with timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second) // 3s handshake timeout
 	defer cancel()
 	quic_conn, err := quic.Dial(ctx, conn, server_addr, tlsConfig, quicConfig)
 	if err != nil {
 		fmt.Println("Error in creating connection", err)
-
 		return nil
 	}
 
-
 	// returning the quic_conn
 	return &QUICConnection{Host: host, Raw: quic_conn, Err: nil, Udpport: conn}
-
-	
-
-
-
 }
 
 // SendHTTPRequest sends an HTTP/3 request over QUIC
